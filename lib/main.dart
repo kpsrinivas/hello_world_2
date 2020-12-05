e81d4bda313f9e4d692539504a4c485d47f83d0c
Srinivas Kolluri <kpsrinivas@heresy.in>
2020-12-05 19:26:41 +0530

    Proider.

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class CurrentUser {
+class CurrentUser with ChangeNotifier {
   String name = "Srinivas";
+
+  setName(String name) {
+    this.name = name;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    var currentUser = new CurrentUser();
-    currentUser.name = "Srinivas K";
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -22,34 +26,52 @@ class MyApp extends StatelessWidget {
 
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: Column(
-        children: [
-          CurrentUserName(cu: currentUser),
-          ChangeUsernameButton(cu: currentUser)
-        ]
-      ),
+      home: ChangeNotifierProvider(
+        create: (_) => new CurrentUser(),
+        child: Column(
+          children: [
+            CurrentUserName(),
+            ChangeUsernameButton()
+          ]
+        ),
+      )
     );
   }
 }
 
 class CurrentUserName extends StatelessWidget {
-  final CurrentUser cu;
-  const CurrentUserName({Key key, this.cu}): super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Text(cu.name);
+    return Column(
+      children: [
+        Consumer<CurrentUser>(
+          builder: (c, currentUser, _) {
+            return Text(currentUser.name);
+          }
+        ),
+        Text("image")
+      ],
+    );
+    // return Consumer<CurrentUser>(
+    //   builder: (c, currentUser, _) {
+    //     return Column(
+    //       children: [
+    //         Text(currentUser.name),
+    //         Text("image")
+    //       ],
+    //     );
+    //   }
+    // );
   }
 }
 
 class ChangeUsernameButton extends StatelessWidget {
-  final CurrentUser cu;
-  const ChangeUsernameButton({Key key, this.cu}): super(key: key);
   @override
   Widget build(BuildContext context) {
     return FlatButton(
       child: Text("Hit me to change name"),
       onPressed: () {
-        cu.name = "Srinivas Kolluri";
+        Provider.of<CurrentUser>(context, listen: false).setName("Srinivas Kolluri");
       },
     );
   }
